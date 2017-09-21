fdd5f979f14cda91effaf72ee8da55db8a03f4f4
Anand Patel <Anand@Anands-MacBook-Pro.local>
2017-09-20 19:17:11 -0500

    Update Manage Page.

--- client/app/scripts/controllers/manage.js
+++ client/app/scripts/controllers/manage.js
@@ -1,7 +1,44 @@
-angular.module('clientApp')
-.controller('manangeCtrl', ['$scope', function($scope) {
+/* jshint ignore:start */
 
-	$scope.profile = function(){
-  		console.log("Open Profile");
-    };
-}]);
+(function () {
+    'use strict';
+
+    angular
+        .module('clientApp')
+        .controller('manageCtrl', manageCtrl);
+
+    manageCtrl.$inject = ['$scope',];
+    function manageCtrl($scope) {
+        $scope.items = [
+        	{
+        		name: 'Manage Profile',
+        		value: 'Profile'
+        	},
+        	{
+        		name: 'Manage Payments',
+        		value: 'Payment'
+        	},
+        	{
+        		name: 'Deals and Referrals',
+        		value: 'Deal'
+        	},
+        	{
+        		name: 'Messages',
+        		value: 'Message'
+        	},
+        	{
+        		name: 'Listings',
+        		value: 'Listing'
+        	}
+        ];
+
+        $scope.selection = $scope.items[0].value;
+
+        $scope.updateChoice = function(choice)
+        {
+        	$scope.selection = choice.value;
+        	console.log('check the selection: ', $scope.selection);
+        }
+    }
+})();
+    /* jshint ignore:end */
